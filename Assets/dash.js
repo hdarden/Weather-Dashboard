b21cf48dfe8c7c11ddb6efb7412352ad6226932a
Helen Darden <hgdarden@gmail.com>
2020-05-12 20:49:59 -0400

    JS searchBar

--- Assets/dash.js
+++ Assets/dash.js
@@ -1,1 +1,14 @@
-console.log("link")
+//console.log("link")
+
+//search bar========================
+
+var searchBar = JSON.parse(localStorage.getItem("searches")) || [];
+
+    function renderHistory(){
+        $("#history").empty();
+
+        for(var i = 0; i < searchBar.length; i ++){
+            $("#history").append($("<p class= 'city'>").text(searchBar[i]));
+
+        }
+    }
